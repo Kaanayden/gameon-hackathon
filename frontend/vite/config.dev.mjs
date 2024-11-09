09b9cb8df0fb56d7d309ded0880bc9b92e9c867c
koybasimuhittin <koybasimuhittin@gmail.com>
2024-11-10 00:20:10 +0300

    initialize vite

---
+++ frontend/vite/config.dev.mjs
@@ -0,0 +1,23 @@
+import basicSsl from '@vitejs/plugin-basic-ssl';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    base: './',
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    phaser: ['phaser']
+                }
+            }
+        },
+    },
+    server: {
+        port: 8080
+    },
+    plugins: [basicSsl()],
+    build: {
+        outDir: './docs'
+      },
+    base: './'    
+});
